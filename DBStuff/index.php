7cb9d1f2fc6bbf81f3b5d643bdc6e318704cfdc5
redlxx <github@redlxx.com>
2014-01-24 20:17:33 -0500

    Added DB Stuff Folder

---
+++ DBStuff/index.php
@@ -0,0 +1,9 @@
+<?php 
+	$username="dbo512305653";
+	$password="TeamAwesome";
+	$database="db512305653";
+	mysql_connect(localhost,$username,$password);
+	@mysql_select_db($database) or die( "Unable to select database");
+	$query="";
+	mysql_query($query);
+?>
