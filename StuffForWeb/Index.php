f474eaa1d5b0ab53d1c49d98a2b724ad4fbbc560
bguggs <bguggs@gmail.com>
2014-01-25 13:59:06 -0500

    Pages

---
+++ StuffForWeb/Index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Activity Picker</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
+</head>
+<body>
+
+<h1> "WHERE THE FUCK IS PETER?!?" </h1>
+<p>"I'm a monkey. Andrew's an idiot. Ra ra ra."</p>
+
+<div style="overflow:hidden;" data-role="footer">
+    <h4 style="text-align:center;">Navigation</h4>
+    <div data-role="navbar">
+        <ul>
+            <li><a href="Index.php" class="ui-btn-active">Home</a></li>
+            <li><a href="About.php">About</a></li>
+            <li><a href="InputForm.php">Submit Activity</a></li>
+        </ul>
+    </div><!-- /navbar -->
+</div><!-- /footer -->
+</body>
+<footer>
+
+</footer>
+</html>
