c5b58824a1f8770a1e8f857c11629ce516315ae2
redlxx <github@redlxx.com>
2014-01-25 11:29:14 -0500

    WORKING DB!
    YAY!

--- DBStuff/index.php
+++ DBStuff/index.php
@@ -1,28 +1,17 @@
 <DOCTYPE! HTML>
 <head>
-	<?php 
-		$username="dbo512305653";
-		$password="TeamAwesome";
-		$database="db512305653";
-		mysql_connect(localhost,$username,$password);
-		@mysql_select_db($database) or die( "Unable to select database");
-		/* Not querying yet
-		$query="";
-		mysql_query($query);*/
-		mysql_close();
-	?>
-	<title>PHP Test Page </title>
+	<title>PHP Test Page</title>
 </head>
 <body>
 	<form action="insert.php" method="post">     
-	Game Name: <input type="text" name="game_name" />
-	Min People: <input type="text" name="MinPeople" />
-	Max People: <input type="text" name="MaxPeople" />
-	User: <input type="text" name="User" />
+	Game Name: <input type="text" name="game_name" /><br>
+	Min People: <input type="text" name="MinPeople" /><br>
+	Max People: <input type="text" name="MaxPeople" /><br>
+	User: <input type="text" name="User" /><br>
 	Drinking: <br><input type="radio" name="Drinking" value="0" /> No <br>
 	<input type="radio" name="Drinking" value="1" /> Yes <br>
-	Rules and Instructions: <input type="text" name="Rules" />
-	Requirements <input type="text" name="Requirements" />
+	Rules and Instructions: <input type="text" name="Rules" /><br>
+	Requirements <input type="text" name="Requirements" /><br>
 	
 	<input type="Submit" /></form>
 </body>
--- DBStuff/insert.php
+++ DBStuff/insert.php
@@ -2,6 +2,7 @@
 	$username="dbo512305653";
 	$password="TeamAwesome";
 	$database="db512305653";
+	$host="db512305653.db.1and1.com";
 	$GamVal=$_POST['game_name'];
 	$MinVal=$_POST['MinPeople'];
 	$MaxVal=$_POST['MaxPeople'];
@@ -10,9 +11,11 @@
 	$WeiVal=$_POST['Weight'];
 	$RulVal=$_POST['Rules'];
 	$ReqVal=$_POST['Requirements'];
-	mysql_connect(localhost,$username,$password);
+	mysql_connect($host,$username,$password);
 	@mysql_select_db($database) or die( "Unable to select database");
-	$query = "INSERT INTO Games_List VALUES('','$GamVal','$MinVal','$MaxVal','$UseVal','$DriVal','$WeiVal','$RulVal','$ReqVal')";
+	$query = "INSERT INTO Games_List (Game_Name, MinPeople, MaxPeople,User, Drinking, Rules, Requirements)
+	VALUES('$GamVal','$MinVal','$MaxVal','$UseVal','$DriVal','$RulVal','$ReqVal')";
+	echo $query;
 	mysql_query($query);
 	mysql_close();
 ?>
